c9ec593124a862b939afe3250c00202d4e01dd65
Rocky Neurock <james.neurock@kloeckner.com>
2019-08-19 00:16:10 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Atom auto-type
+
+An Atom package that automatically types content from a file into the active
+text editor.
---
+++ spec/auto-type-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AutoTypeView from '../lib/auto-type-view';
+
+describe('AutoTypeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
